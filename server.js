632f219e7832ffc5927bbaef15186bd8b536ace0
sam_wilson_s_s <83699105+Wilson-hue@users.noreply.github.com>
2024-02-04 22:03:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let mailTransporter = nodemailer.createTransport({
 
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/turf', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI;, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('Connected to database successfully');
 })
